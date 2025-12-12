bd0c6b1464d50ac5056357b43902e29f64af9920
Nguyen Minh Phu <phu20032k1@gmail.com>
2025-12-12 14:25:06 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.post("/stt", upload.single("audio"), async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("STT server running on port", PORT);
 });
